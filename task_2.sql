2e74c5bfc4d49401c8cc1d0a3a17cbd31c552a5c
nndwelenis <nndwelenis@gmail.com>
2025-12-14 16:39:00 +0200

    Fix table name casing for checker

--- task_2.sql
+++ task_2.sql
@@ -1,40 +1,63 @@
+-- CREATE DATABASE
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- SELECT DATABASE
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS AUTHORS (
+-- AUTHORS TABLE
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS BOOKS (
+-- BOOKS TABLE
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    CONSTRAINT fk_books_authors
+        FOREIGN KEY (author_id)
+        REFERENCES Authors(author_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS CUSTOMERS (
+-- CUSTOMERS TABLE
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS ORDERS (
+-- ORDERS TABLE
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    CONSTRAINT fk_orders_customers
+        FOREIGN KEY (customer_id)
+        REFERENCES Customers(customer_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+-- ORDER DETAILS TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    CONSTRAINT fk_orderdetails_orders
+        FOREIGN KEY (order_id)
+        REFERENCES Orders(order_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_orderdetails_books
+        FOREIGN KEY (book_id)
+        REFERENCES Books(book_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
